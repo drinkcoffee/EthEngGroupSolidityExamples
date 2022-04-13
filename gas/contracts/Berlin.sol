10a71f858cfc1dc5bae2eb029e659ea982f6318e
Peter Robinson <drinkcoffee@eml.cc>
2022-04-13 15:44:34 +1000

    gas examples
    Signed-off-by: Peter Robinson <drinkcoffee@eml.cc>

---
+++ gas/contracts/Berlin.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8 <0.9.0;
+
+contract Berlin {
+  uint256 public a1;
+  uint256 public a2;
+  uint256 public a3;
+
+
+  constructor() {
+    a1 = 1;
+  }
+
+  function singleLoad() external {
+    if (a1 == 2) {
+      // Will never happen. Forces this to be a transaction function.
+      a1 = 3;
+    }
+  }
+
+  function twoLoad() external {
+    if (a1 == 2) {
+      // Will never happen. Forces this to be a transaction function.
+      a1 = 3;
+    }
+    dummy2();
+  }
+  function dummy2() internal {
+    if (a1 == 2) {
+       revert("Bang!");
+     }
+  }
+
+}
