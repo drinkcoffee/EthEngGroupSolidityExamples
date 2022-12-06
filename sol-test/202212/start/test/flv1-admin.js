c53edb4b1d16b0cbffd06ff0ef9db0f39d8b1a0e
cds-amal <cds.sudama@gmail.com>
2022-12-05 22:07:47 -0500

    flv1-admin
    - add truffle-assertions
    - pull before and assertions to test

--- sol-test/202212/start/test/flv1-admin.js
+++ sol-test/202212/start/test/flv1-admin.js
@@ -1,22 +1,34 @@
 // SPDX-License-Identifier: MIT
+const FlashLoanV1 = artifacts.require("./FlashLoanV1.sol");
+const truffleAssert = require('truffle-assertions');
 
 contract('FlashLoanV1, Admin', function(accounts) {
-    let common = require('./common');
-    let commonAdmin = require('./common-admin')
+    let flashLoanContract;
 
+    beforeEach(async function () {
+        // Interest rate is 1000, which is 0.1%
+        flashLoanContract = await FlashLoanV1.new(1000);
+    });
+    
     it("admin after initialise", async function() {
-        let flashLoanContract = await common.getFlashLoanV1();
-        await commonAdmin.adminAfterInitialise(flashLoanContract, accounts);
+        const admin = await flashLoanContract.admin.call();
+        assert.equal(admin, accounts[0], "Unexpectedly, deployer not owner");
     });
 
     it("transfer ownership", async function() {
-        let flashLoanContract = await common.getFlashLoanV1();
-        await commonAdmin.transferOwnership(flashLoanContract, accounts);
+        // Admin should now be accounts[0]
+        await flashLoanContract.transferOwnership(accounts[1]); // Note: default transaction signer is accounts[0]
+
+        const admin = await flashLoanContract.admin.call();
+        assert.equal(admin, accounts[1], "Unexpectedly, ownership not changed");        
     });
 
     it("transfer ownership access control", async function() {
-        let flashLoanContract = await common.getFlashLoanV1();
-        await commonAdmin.transferOwnershipAccessControl(flashLoanContract, accounts);
+        await truffleAssert.fails(
+            flashLoanContract.transferOwnership(accounts[1],  {from: accounts[1]}),
+            truffleAssert.ErrorType.REVERT,
+            "Not admin!"
+        );
     });
 
 });
