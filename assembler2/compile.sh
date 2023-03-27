70213d2bcc38b50fa624c6f27f75acf206f88618
Peter Robinson <drinkcoffee@eml.cc>
2023-03-27 14:41:01 +1000

    Added simple proxy in Solidity

--- assembler2/compile.sh
+++ assembler2/compile.sh
@@ -1,2 +1,5 @@
 solc --strict-assembly contracts/TransparentProxy.yul --optimize --bin
 
+# solc contracts/Proxy.sol --bin --asm --no-cbor-metadata --optimize --optimize-runs=1000
+solc contracts/Proxy.sol --bin-runtime --no-cbor-metadata --optimize --optimize-runs=1000
+# solc contracts/Proxy.sol --bin --no-cbor-metadata --optimize --optimize-runs=1000
