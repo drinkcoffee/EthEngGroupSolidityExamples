ae555f972ab3aa0bf11377d2a926550ebd0b389e
Peter Robinson <drinkcoffee@eml.cc>
2023-05-24 18:06:00 +1000

    Add info about extra bug

--- ai/202304/start-annotated/contracts/Inefficient.sol
+++ ai/202304/start-annotated/contracts/Inefficient.sol
@@ -7,6 +7,8 @@ contract Inefficient {
     // TODO significantly more efficient to have this as a map
     address[] public auth;
 
+    // TODO need to add an initial admin in constructor, otherwise authorise will always fail.
+
     modifier ifAuth() {
         bool authorised = false;
         for (uint256 i = 0; i < auth.length; i++) {
