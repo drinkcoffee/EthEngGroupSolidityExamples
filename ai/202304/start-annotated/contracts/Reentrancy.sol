06f1d4304a40b72d0010f6480166d6dbd83e83cc
Peter Robinson <drinkcoffee@eml.cc>
2023-05-30 16:03:05 +1000

    More sol code

--- ai/202304/start-annotated/contracts/Reentrancy.sol
+++ ai/202304/start-annotated/contracts/Reentrancy.sol
@@ -11,48 +11,13 @@ interface FlashLoanReceiverInterface {
 abstract contract Reentrancy {
     uint256 constant INTEREST_DIVISOR = 1000000;
 
-    // Minimum period a deposit must be held, in blocks.
-    uint256 constant MIN_HOLD_PERIOD = 1000;
-
-    // Minimum number of blocks between calling setInterestRate and changeInterestRate.
-    uint256 constant MIN_INTEREST_RATE_CHANGE_PERIOD = 1000;
-
     event FlashLoan(uint256 _balBefore, uint256 _fee,  uint256 _profit);
-
-    // TODO: Advanced: Gas Saving: To save gas, only store a hash, and pass in params as calldata
-    struct DepositStruct {
-        address beneficiary;
-        uint256 amount;
-        uint256 interestRate;
-        uint256 depositBlockNumber;
-    }
-    mapping (uint256 => DepositStruct) public accounts;
 
     // Current interest rate per block.
     uint256 public interestRatePerBlock;
-    // The forthcoming interest rate.
-    uint256 public nextInterestRatePerBlock;
-    // Block number when nextInterestRate will take effect.
-    uint256 public interestRateChangeBlock;
 
     // Interest earned that hasn't been distributed
     uint256 public profit;
-
-    // A combination of time (number of blocks) and amount deposited, for all deposits.
-    // Used to work out how much to pay out.
-    // The deposit volume is as at the lastDepositWithdrawalBlock. It is recalculated
-    // when deposit or payout are called.
-    uint256 public depositVolume;
-
-    // The amount of Eth deposited in the contract.
-    uint256 public totalDepositValue;
-
-    // The last block that the deposit volume has been recalculated for.
-    uint256 public lastDepositWithdrawalBlock;
-
-    // TODO  Reentrancy check
-    // TODO bool inFlashLoan;
-
 
     address private owner;
 
@@ -61,7 +26,6 @@ abstract contract Reentrancy {
     }
 
 
-    // TODO Basic: should have authentication
     function setInterestRate(uint256 _rate) external {
         require(owner == msg.sender, "Not authorised");
         interestRatePerBlock = _rate;
---
+++ ai/scripts/README.md
@@ -0,0 +1,17 @@
+To run the script first install:
+
+pip3 install openai
+pip3 install env
+pip3 install python-dotenv
+
+Have your OpenAI API key in an environment variable: OPENAI_API_KEY
+export OPENAI_API_KEY=<yourkey>
+
+Install truffle and truffle-flattener
+
+Flattern the file you want to analyse:
+truffle-flattener contracts/functioncall/gpact/GpactCrosschainControl.sol > flat.sol
+
+Execute the review:
+
+python3 reviewsol.py
