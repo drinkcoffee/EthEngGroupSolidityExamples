9ce9f4bc54475a974fff3abb69021df3ccab463e
Peter Robinson <drinkcoffee@eml.cc>
2023-12-14 15:41:32 +1000

    Fixed proxy

--- sol-test/202312/nftcontract/src/UpgradeProxy.sol
+++ sol-test/202312/nftcontract/src/UpgradeProxy.sol
@@ -1,16 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 
-contract UpgradeProxy is TransparentUpgradeProxy {
-    constructor(address _logic, address initialOwner) {
-        bytes memory data = bytes();
-        super(_logic, initialOwner, data);
-    }
-
-    function __proxyAdmin() external view {
-        return _proxyAdmin();
+contract UpgradeProxy is TransparentUpgradeableProxy {
+    constructor(address _logic, address initialOwner) TransparentUpgradeableProxy(_logic, initialOwner, bytes("")) {
     }
 }
