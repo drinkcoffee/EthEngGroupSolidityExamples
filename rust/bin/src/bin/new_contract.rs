9e06445ccb40a68260a90b04948662ce6b6c2fcc
sergerad <serge.radinovich@immutable.com>
2025-01-22 18:21:42 +1300

    Add more logs

--- rust/bin/src/bin/new_contract.rs
+++ rust/bin/src/bin/new_contract.rs
@@ -24,6 +24,7 @@ async fn main() -> eyre::Result<()> {
     tracing_subscriber::fmt::init();
 
     // Read the configuration.
+    info!("Reading configuration");
     let config = toml::from_str::<Config>(load_str!("../../../config.toml"))?;
 
     // Create the wallet.
@@ -32,22 +33,29 @@ async fn main() -> eyre::Result<()> {
     let wallet = EthereumWallet::new(signer);
 
     // Create RPC provider.
+    let url = Url::parse("https://rpc.testnet.immutable.com")?;
+    info!("Creating RPC provider for: {url}");
     let provider = ProviderBuilder::new()
         .with_recommended_fillers()
         .wallet(wallet)
-        .on_http(Url::parse("https://rpc.testnet.immutable.com")?);
+        .on_http(url);
 
     // Deploy the contract.
-    info!("Deploying contract");
+    info!("Deploying Counter contract");
     let contract = CounterDeploy::new(provider, U256::from(42u64)).await?;
     let contract_address = contract.address().await?;
-    info!("Deployed to: {}", contract_address);
+    info!("Deployed to: {contract_address}");
 
     // Use the contract.
+    info!("Calling Counter::number()");
     let number = contract.number().await?;
     info!("Number initial value: {}", number);
+
+    info!("Calling Counter::increment()");
     let txhash = contract.increment().await?;
     info!("Increment tx hash: {}", txhash);
+
+    info!("Calling Counter::number()");
     let number = contract.number().await?;
     info!("Number after increment: {}", number);
 
